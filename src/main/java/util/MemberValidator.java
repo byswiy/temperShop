32348c8f5a438f3570192ccdc21a3be16a61ebd8
byswiy <byswiy99@naver.com>
2022-06-14 22:43:49 +0900

    Validator 수정

--- src/main/java/util/MemberValidator.java
+++ src/main/java/util/MemberValidator.java
@@ -1,5 +1,7 @@
 package util;
 
+import vo.MemberInfo;
+
 // 회원 정보 파라미터 
 // 아이디 파라미터 - 길이: 6 ~ 16자로 제한
 //					형태: 영어 대문자, 소문자, 숫자로 이루어질 수 있고 대문자는 필수가 아니다
@@ -15,12 +17,14 @@ package util;
 
 // 이메일 규칙 - 반드시 @와 .을 포함하도록 한다
 public class MemberValidator {
-	public boolean idVali(String id) {
+	public boolean allValidator(MemberInfo memberInfo) {
 		
+		// 아이디 파라미터
 		boolean correctId = false;
 		
 		// 시작은 반드시 영소문자로 시작하며 영대소문자, 숫자로 이루어진 6~16자 아이디 정규식
-		correctId = id.matches("^[a-z]{1}[A-Za-z0-9]{5,15}$");  
+		correctId = memberInfo.getId().matches("^[a-z]{1}[A-Za-z0-9]{5,15}$");
+		
 		if(!correctId) {
 			return false;
 		}
